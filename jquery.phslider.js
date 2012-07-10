f8bcaf522e2fbc4e87a13d8a853683ba366be208
Pyronhell <pyronhell@gmail.com>
2012-07-10 10:47:27 +0200

    Code beautify

--- jquery.phslider.js
+++ jquery.phslider.js
@@ -1,14 +1,15 @@
-;(function ( $, window, document, undefined ) {
+;
+(function ($, window, document, undefined) {
     // Create the defaults once
     var pluginName = 'phslider',
         defaults = {
-            width: 0,
-            height: 0,
+            width:0,
+            height:0,
             // Seconds
-            duration: 1,
-            animation: 'fade',
+            duration:1,
+            animation:'fade',
             // Miliseconds
-            animationDuration: 2000
+            animationDuration:2000
         },
         actualSlide = 1,
         nextSlide = 1,
@@ -17,10 +18,10 @@
         _styles = null;
 
     // The actual plugin constructor
-    function Plugin( element, options ) {
+    function Plugin(element, options) {
         this.element = element;
 
-        this.options = $.extend( {}, defaults, options) ;
+        this.options = $.extend({}, defaults, options);
 
         this._defaults = defaults;
         this._name = pluginName;
@@ -31,8 +32,8 @@
     Plugin.prototype.init = function () {
         // Getting slides info
         slidesNumber = $(this.element).children('.slide').length
-        $(this.element).children('.slide').each(function() {
-            $(this).attr('data-slide', $(this).index()+1)
+        $(this.element).children('.slide').each(function () {
+            $(this).attr('data-slide', $(this).index() + 1)
         })
 
         // Getting viewport width/height
@@ -50,150 +51,150 @@
         this.start();
     };
 
-    Plugin.prototype.prepareStyles = function() {
+    Plugin.prototype.prepareStyles = function () {
         _styles = {
-            first: {
-                display: 'block'
+            first:{
+                display:'block'
             },
-            reset: {
-                opacity: 1
+            reset:{
+                opacity:1
                 //'z-index': 0
             },
-            slidetop: {
-                actualSlide: {
-                    position: 'absolute'
+            slidetop:{
+                actualSlide:{
+                    position:'absolute'
                 },
-                actualSlideAnimationTo: {
-                    top: -1 * this.getViewportHeight()
+                actualSlideAnimationTo:{
+                    top:-1 * this.getViewportHeight()
                 },
-                nextSlide: {
-                    display: 'block',
-                    left: 0,
-                    position: 'absolute',
-                    top: this.getViewportHeight()
+                nextSlide:{
+                    display:'block',
+                    left:0,
+                    position:'absolute',
+                    top:this.getViewportHeight()
                 },
-                nextSlideAnimationTo: {
-                    top: 0
+                nextSlideAnimationTo:{
+                    top:0
                 }
             },
-            slideright: {
-                actualSlide: {
-                    position: 'absolute'
+            slideright:{
+                actualSlide:{
+                    position:'absolute'
                 },
-                actualSlideAnimationTo: {
-                    left: this.getViewportWidth()
+                actualSlideAnimationTo:{
+                    left:this.getViewportWidth()
                 },
-                nextSlide: {
-                    display: 'block',
-                    left: -1 * this.getViewportWidth(),
-                    position: 'absolute',
-                    top: 0
+                nextSlide:{
+                    display:'block',
+                    left:-1 * this.getViewportWidth(),
+                    position:'absolute',
+                    top:0
                 },
-                nextSlideAnimationTo: {
-                    left: 0
+                nextSlideAnimationTo:{
+                    left:0
                 }
             },
-            slidebottom: {
-                actualSlide: {
-                    position: 'absolute'
+            slidebottom:{
+                actualSlide:{
+                    position:'absolute'
                 },
-                actualSlideAnimationTo: {
-                    top: this.getViewportHeight()
+                actualSlideAnimationTo:{
+                    top:this.getViewportHeight()
                 },
-                nextSlide: {
-                    display: 'block',
-                    left: 0,
-                    position: 'absolute',
-                    top: -1 * this.getViewportHeight()
+                nextSlide:{
+                    display:'block',
+                    left:0,
+                    position:'absolute',
+                    top:-1 * this.getViewportHeight()
                 },
-                nextSlideAnimationTo: {
-                    top: 0
+                nextSlideAnimationTo:{
+                    top:0
                 }
             },
-            slideleft: {
-                actualSlide: {
-                    position: 'absolute'
+            slideleft:{
+                actualSlide:{
+                    position:'absolute'
                 },
-                actualSlideAnimationTo: {
-                    left: -1 * this.getViewportWidth()
+                actualSlideAnimationTo:{
+                    left:-1 * this.getViewportWidth()
                 },
-                nextSlide: {
-                    display: 'block',
-                    left: this.getViewportWidth(),
-                    position: 'absolute',
-                    top: 0
+                nextSlide:{
+                    display:'block',
+                    left:this.getViewportWidth(),
+                    position:'absolute',
+                    top:0
                 },
-                nextSlideAnimationTo: {
-                    left: 0
+                nextSlideAnimationTo:{
+                    left:0
                 }
             },
-            fade: {
-                actualSlide: {
-                    position: 'absolute',
-                    display: 'block',
-                    left: 0,
-                    top: 0,
-                    opacity: 1,
-                    'z-index': 10
+            fade:{
+                actualSlide:{
+                    position:'absolute',
+                    display:'block',
+                    left:0,
+                    top:0,
+                    opacity:1,
+                    'z-index':10
                 },
-                actualSlideAnimationTo: {
-                    opacity: 0
+                actualSlideAnimationTo:{
+                    opacity:0
                 },
-                nextSlide: {
-                    position: 'absolute',
-                    display: 'block',
-                    left: 0,
-                    top: 0,
-                    opacity: 0,
-                    'z-index': 1
+                nextSlide:{
+                    position:'absolute',
+                    display:'block',
+                    left:0,
+                    top:0,
+                    opacity:0,
+                    'z-index':1
                 },
-                nextSlideAnimationTo: {
-                    opacity: 1
+                nextSlideAnimationTo:{
+                    opacity:1
                 }
             }
         };
     }
 
-    Plugin.prototype.createViewport = function() {
+    Plugin.prototype.createViewport = function () {
         $(this.element).css({
-            height: this.options.height,
-            position: 'relative',
-            overflow: 'hidden',
-            width: this.options.width
+            height:this.options.height,
+            position:'relative',
+            overflow:'hidden',
+            width:this.options.width
         })
     }
 
-    Plugin.prototype.prepareSlides = function() {
+    Plugin.prototype.prepareSlides = function () {
         for (var i = 1; i <= slidesNumber; i++) {
             $(this.element).children('[data-slide="' + i + '"]').css({
-                width: this.options.width,
-                height: this.options.height,
-                display: 'none'
+                width:this.options.width,
+                height:this.options.height,
+                display:'none'
             });
         }
         $(this.element).children('[data-slide="1"]').css(_styles.first);
     }
 
-    Plugin.prototype.start = function() {
+    Plugin.prototype.start = function () {
         this.getNextSlide();
         this.getDataFromSlide(actualSlide)
         this.waitForNextSlide()
     }
 
-    Plugin.prototype.waitForNextSlide = function() {
+    Plugin.prototype.waitForNextSlide = function () {
         var waitTime = $actualSlide.duration;
         setTimeout($.proxy(this.next, this), waitTime * 1000);
     }
 
-    Plugin.prototype.slideTo = function(_slide) {
+    Plugin.prototype.slideTo = function (_slide) {
         this.animate();
-        actualSlide =_slide;
+        actualSlide = _slide;
         this.getDataFromSlide(actualSlide);
         this.getNextSlide();
         this.waitForNextSlide();
     }
 
-    Plugin.prototype.animate = function() {
+    Plugin.prototype.animate = function () {
         var _actual = $(this.element).children('[data-slide="' + actualSlide + '"]');
         var _next = $(this.element).children('[data-slide="' + nextSlide + '"]');
 
@@ -207,11 +208,11 @@
         _next.css(_styles.reset);
     }
 
-    Plugin.prototype.next = function() {
+    Plugin.prototype.next = function () {
         this.slideTo(nextSlide)
     }
 
-    Plugin.prototype.getNextSlide = function() {
+    Plugin.prototype.getNextSlide = function () {
         var next = (actualSlide + 1);
         if (!$(this.element).children('[data-slide="' + next + '"]').length) {
             next = 1;
@@ -219,32 +220,32 @@
         nextSlide = next;
     }
 
-    Plugin.prototype.getDataFromSlide = function(slide) {
+    Plugin.prototype.getDataFromSlide = function (slide) {
         var _element = $(this.element).children('[data-slide="' + slide + '"]');
         $actualSlide = {
-            'slide': _element.attr('data-slide'),
-            'duration': (_element.attr('data-duration')) ? _element.attr('data-duration') : this.options.duration,
-            'animation': (_element.attr('data-animation')) ? _element.attr('data-animation') : this.options.animation,
-            'animationDuration': (_element.attr('data-animation-duration')) ? _element.attr('data-animation-duration') : this.options.animationDuration
+            'slide':_element.attr('data-slide'),
+            'duration':(_element.attr('data-duration')) ? _element.attr('data-duration') : this.options.duration,
+            'animation':(_element.attr('data-animation')) ? _element.attr('data-animation') : this.options.animation,
+            'animationDuration':(_element.attr('data-animation-duration')) ? _element.attr('data-animation-duration') : this.options.animationDuration
         };
     }
 
-    Plugin.prototype.getViewportWidth = function() {
+    Plugin.prototype.getViewportWidth = function () {
         return this.options.width;
     }
 
-    Plugin.prototype.getViewportHeight = function() {
+    Plugin.prototype.getViewportHeight = function () {
         return this.options.height;
     }
 
     // A really lightweight plugin wrapper around the constructor,
     // preventing against multiple instantiations
-    $.fn[pluginName] = function ( options ) {
+    $.fn[pluginName] = function (options) {
         return this.each(function () {
             if (!$.data(this, 'plugin_' + pluginName)) {
-                $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+                $.data(this, 'plugin_' + pluginName, new Plugin(this, options));
             }
         });
     }
 
-})( jQuery, window, document );
+})(jQuery, window, document);
